94d4a1052a62e2cfe82147f94de0875723c7b326
LuckyCX <721685743@qq.com>
2021-09-02 10:39:17 +0800

    学生端 行为与评语样式实现

--- pages/stu_actionComment/index.vue
+++ pages/stu_actionComment/index.vue
@@ -8,90 +8,31 @@
 			<view v-if="current === 0">
 				<view class="wrap"> 
 					<u-time-line>
-						<u-time-line-item nodeTop="2">
-							<template v-slot:node>
-								<view class="u-node" style="background: #19be6b;">
-									<u-icon name="pushpin-fill" color="#fff" :size="24"></u-icon>
-								</view>
-							</template>
+						<u-time-line-item :key="index" nodeTop="10" v-for="(item,index) in pagedata0">
 							<template v-slot:content>
-								<view>
-									<view class="u-order-time">2019-05-08 12:12</view>
-									<view class="u-order-title">待取件</view>
-									<view class="u-order-desc">[自提柜]您的快件已放在楼下侧门，直走前方53.6米，左拐约10步，再右拐直走，见一红灯笼停下，叩门三下，喊“芝麻开门”即可。</view>
+								<view v-if="item.student_behavior_id!=null"><!-- 行为项目文本不为空说明有行为记录 -->
+									<view class="u-order-time">{{item.order_time}}</view>
+									<view class="u-order-title">{{item.item_txt}}</view>
+									<view class="u-order-time">{{item.behavior_time}}</view>
+									<view class="u-order-time">{{item.day}} {{item.class_node}} {{item.sub_name}}</view>
+									<view class="u-order-detail">{{item.comment}}</view>
+									<template v-if="item.bimgs.length>0">
+										<view class="choose-file" style="padding: 0 0 6px 0 !important;margin-top: 10px;">
+											<g-upload  ref="upload2" :mode="item.bimgs" :deleteBtn='deleteBtn' :control='control' :columnNum="columnNum"></g-upload>
+										</view>
+									</template>
+									<view class="u-order-right-txt"><view class="u-order-right">记</view>{{item.behavior_recorder}}</view>
 								</view>
-							</template>
-						</u-time-line-item>
-						<u-time-line-item nodeTop="2">
-							<template v-slot:node>
-								<view class="u-node" >
-									<u-icon name="account-fill" color="#fff" :size="24"></u-icon>
-								</view>
-							</template>
-							<template v-slot:content>
-								<view>
-									<view class="u-order-title unacive">派送中</view>
-									<view class="u-order-desc">【深圳市】快件已到达目的地，派件员为国产锦衣卫007号，电话：<text class="tel">13833882438</text>，请留意快递信息</view>
-									<view class="u-order-time">2019-05-08 06:03</view>
-								</view>
-							</template>
-						</u-time-line-item>
-						<u-time-line-item nodeTop="2">
-							<template v-slot:node>
-								<view class="u-node">
-									<u-icon name="car-fill" color="#fff" :size="24"></u-icon>
-								</view>
-							</template>
-							<template v-slot:content>
-								<view>
-									<view class="u-order-title unacive">运输中</view>
-									<view class="u-order-desc">【深圳市】快递已到达 深圳固戍一部</view>
-									<view class="u-order-time">2019-05-07 08:05</view>
-								</view>
-							</template>
-						</u-time-line-item>
-						<u-time-line-item>
-							<template v-slot:content>
-								<view>
-									<view class="u-order-desc">【深圳市】快件已从深圳运转中心发出，正在发往深圳宝安一部</view>
-									<view class="u-order-time">2019-12-06 22:30</view>
-								</view>
-							</template>
-						</u-time-line-item>
-						<u-time-line-item>
-							<template v-slot:content>
-								<view>
-									<view class="u-order-desc">【深圳市】快件已到达 深圳运转中心</view>
-									<view class="u-order-time">2019-12-04 16:42</view>
-								</view>
-							</template>
-						</u-time-line-item>
-						<u-time-line-item>
-							<template v-slot:content>
-								<view>
-									<view class="u-order-desc">【郑州市】快件已从郑州运转中心出发，正在发往深圳运转中心</view>
-									<view class="u-order-time">2019-12-02 12:55</view>
-								</view>
-							</template>
-						</u-time-line-item>
-						<u-time-line-item>
-							<template v-slot:content>
-								<view>
-									<view class="u-order-desc">【郑州市】快件已到达 郑州运转中心</view>
-									<view class="u-order-time">2019-12-02 08:23</view>
-								</view>
-							</template>
-						</u-time-line-item>
-						<u-time-line-item nodeTop="0">
-							<template v-slot:node>
-								<view class="u-node">
-									<u-icon name="file-text-fill" color="#fff" :size="24"></u-icon>
-								</view>
-							</template>
-							<template v-slot:content>
-								<view>
-									<view class="u-order-desc">您购买的商品【尚方宝剑，先斩后奏】，经由北京军区仓库发货，国内快递承运人【中南海保镖】。</view>
-									<view class="u-order-time">2019-12-01 07:00</view>
+								<view v-if="item.id!=null"><!-- 谈话文本不为空说明有谈话记录 -->
+									<view class="u-order-title2">谈话</view>
+									<view class="u-order-time">{{item.chat_time&&item.chat_time.split(' ')[0]}}</view>
+									<view class="u-order-detail">{{item.chat_detail}}</view>
+									<template v-if="item.aimgs.length>0">
+										<view class="choose-file" style="padding: 0 0 6px 0 !important;margin-top: 10px;">
+											<g-upload ref="upload1" :mode="item.aimgs" :deleteBtn='deleteBtn' :control='control' :columnNum="columnNum"></g-upload>
+										</view>
+									</template>
+									<view class="u-order-right-txt"><view class="u-order-right">谈</view>{{item.create_user_name}}</view>
 								</view>
 							</template>
 						</u-time-line-item>
@@ -100,20 +41,23 @@
 				<uni-load-more :status="pageobj0.status" :icon-size="17" :content-text="pageobj0.contentText" />
 			</view>
 			<view v-if="current === 1">
-				<!-- <uni-list :border="false">
-					<uni-list-item showArrow :key="index" v-for="(item,index) in pagedata1" :border="true">
-						<text slot="body" class="slot-box slot-text" @click="toDetails(1,item)">
+				<uni-list :border="false">
+					<uni-list-item :key="index" v-for="(item,index) in pagedata1" :border="true">
+						<text slot="body" class="slot-box slot-text">
 							<uni-row>
-								<uni-col :span="24"><view class="title-text">{{item.comment?item.comment:'暂无内容'}}</view></uni-col>
-								<uni-col :span="24"><view class="detail-text">{{item.begin_time}} ~ {{item.end_time}}</view></uni-col>
-								<uni-col :span="24"><view class="detail-text">{{item.grd_name}} {{item.cls_name}}&ensp;{{item.stu_name}}</view></uni-col>
-								<uni-col :span="20"><view class="detail-text">{{item.apply_time}}</view></uni-col>
-								<uni-col :span="4"><view class="detail-text leaveType">{{item.item_code=='sickLeave'?'病假':item.item_code=='absenceLeave'?'事假':''}}</view></uni-col>
+								<uni-col style="display: flex;align-items: center;">
+									<uni-col v-if="item.remark_type=='termRemark'" :span="5"><view class="u-order-title1" style="background: #FF6666;margin-left: 0px;">{{item.remark_type_txt}}</view></uni-col><!-- 期评 -->
+									<uni-col v-if="item.remark_type=='monthRemark'" :span="5"><view class="u-order-title1" style="background: #5BBA5B;margin-left: 0px;">{{item.remark_type_txt}}</view></uni-col><!-- 月评 -->
+									<uni-col v-if="item.remark_type=='weekRemark'" :span="5"><view class="u-order-title1" style="background: #00CFBD;margin-left: 0px;">{{item.remark_type_txt}}</view></uni-col><!-- 周评 -->
+									<uni-col :span="19"><view class="u-order-time">{{item.year}} 学年 {{item.term_name}}</view></uni-col>
+								</uni-col>
+								<uni-col :span="19" :offset="5"><view class="u-order-detail">{{item.remark}}</view></uni-col>
+								<uni-col :span="24" style="text-align: right;"><view class="u-order-time">{{item.create_user_name}}</view></uni-col>
+								<uni-col :span="24" style="text-align: right;"><view class="u-order-time">{{item.create_time}}</view></uni-col>
 							</uni-row>
 						</text>
 					</uni-list-item>
-				</uni-list> -->
-				 4678
+				</uni-list>
 				<uni-load-more :status="pageobj1.status" :icon-size="17" :content-text="pageobj1.contentText" />
 			</view>
 		</view>
@@ -124,6 +68,9 @@
 <script>
 	import util from '../../commom/util.js';
 	import mynavBar from '@/components/my-navBar/m-navBar';
+	// 七牛上传相关
+	 import gUpload from "@/components/g-upload/g-upload.vue"
+	 import cloudFileUtil from '../../commom/uploadFiles/CloudFileUtil.js';
 	let _this;
 	export default {
 		data() {
@@ -133,7 +80,8 @@
 				tabBarItem: {},
 				items: ['行为与谈话', '教师评语'],
 				current: 0,
-				pageSize:15,
+				pageSize0:5,
+				pageSize1:15,
 				pageobj0:{
 					loadFlag:0,//0 刷新 1加载更多
 					page_number:1,//页码
@@ -145,6 +93,13 @@
 					},
 					canload:true,//是否加载更多
 				},
+				
+				// 附件上传相关👇
+				control:false,//是否显示上传 + 按钮 一般用于显示
+				deleteBtn:false,//是否显示删除 按钮 一般用于显示
+				columnNum:3,//每行显示的图片数量
+				imgList: [],//选择的或服务器回传的图片地址，如果是私有空间，需要先获取token再放入，否则会预览失败
+				
 				pageobj1:{
 					loadFlag:0,//0 刷新 1加载更多
 					page_number:1,//页码
@@ -161,7 +116,8 @@
 			}
 		},
 		components: {
-			mynavBar
+			mynavBar,
+			 gUpload
 		},
 		methods: {
 			onClickItem(e) {
@@ -170,21 +126,97 @@
 				}
 				if(this.current===0){
 					if(this.pagedata0.length===0){
-					 	// this.showLoading()
-					 	// this.pageobj0.loadFlag=0
-					 	// this.pageobj0.canload=true
-					 	// this.pageobj0.page_number=1
-					 	// this.getList0()
+					 	this.showLoading()
+					 	this.pageobj0.loadFlag=0
+					 	this.pageobj0.canload=true
+					 	this.pageobj0.page_number=1
+					 	this.getList0()
 					}
 				}else if(this.current===1){
 					if(this.pagedata1.length===0){
-						// this.showLoading()
-						// this.pageobj1.loadFlag=0
-						// this.pageobj1.canload=true
-						// this.pageobj1.page_number=1
-						// this.getList1()
+						this.showLoading()
+						this.pageobj1.loadFlag=0
+						this.pageobj1.canload=true
+						this.pageobj1.page_number=1
+						this.getList1()
 					}
 				}
+			},
+			getList0(){
+				let user_code=this.personInfo.user_code;
+				if(this.personInfo.type_code=='YHLX0004'){
+					user_code=personal.stu_code;
+				}
+				let comData={
+					stu_code:user_code,
+					page_number: this.pageobj0.page_number, //当前页数
+					page_size: this.pageSize0, //每页记录数
+					index_code: this.index_code,
+				}
+				this.post(this.globaData.PARENTS_ATTENDANCE+'behavior/page',comData,response=>{
+				    console.log("responseaaa: " + JSON.stringify(response));
+				    setTimeout(function () {
+				    	uni.stopPullDownRefresh();
+				    }, 1000);
+				    this.hideLoading()
+					//png、jpg、gif/bmp/jpeg
+					response.list.map(item=>{
+						let aimgs=[],bimgs=[];
+						item.asset_ids.map(aitem=>{
+							if(aitem.ext=='png' ||aitem.ext=='jpg'||aitem.ext=='jpeg'||aitem.ext=='gif'||aitem.ext=='bmp'){
+								aimgs.push(aitem.url)
+							}
+						})
+						item.aimgs=aimgs;
+						item.behavior_asset_ids.map(bitem=>{
+							if(bitem.ext=='png' ||bitem.ext=='jpg'||bitem.ext=='jpeg'||bitem.ext=='gif'||bitem.ext=='bmp'){
+								bimgs.push(bitem.url)
+							}
+						})
+						item.bimgs=bimgs;
+					})
+				    if(this.pageobj0.loadFlag===0){
+				    	this.pagedata0=[].concat(response.list)
+				    }else{
+				    	this.pagedata0=this.pagedata0.concat(response.list)
+				    }
+				    if(this.pageobj0.page_number>=response.total_page){
+				    	this.pageobj0.status = 'noMore';
+				    	this.pageobj0.canload=false
+				    }else{
+				    	this.pageobj0.status = 'more';
+				    }
+				})
+			},
+			getList1(){
+				let user_code=this.personInfo.user_code;
+				if(this.personInfo.type_code=='YHLX0004'){
+					user_code=personal.stu_code;
+				}
+				let comData={
+					stu_code:user_code,
+					page_number: this.pageobj1.page_number, //当前页数
+					page_size: this.pageSize1, //每页记录数
+					index_code: this.index_code,
+				}
+				this.post(this.globaData.PARENTS_ATTENDANCE+'remark/page',comData,response=>{
+				    console.log("responseaaa: " + JSON.stringify(response));
+				    setTimeout(function () {
+				    	uni.stopPullDownRefresh();
+				    }, 1000);
+				    this.hideLoading()
+				    if(this.pageobj1.loadFlag===0){
+				    	this.pagedata1=[].concat(response.list)
+				    }else{
+				    	this.pagedata1=this.pagedata1.concat(response.list)
+				    }
+				    if(this.pageobj1.page_number>=response.total_page){
+				    	this.pageobj1.status = 'noMore';
+				    	this.pageobj1.canload=false
+				    }else{
+				    	this.pageobj1.status = 'more';
+				    }
+				})
 			},
 		},
 		onLoad() {
@@ -198,15 +230,8 @@
 			this.tabBarItem = util.getTabbarMenu();
 			this.index_code=this.tabBarItem.access.split("#")[1]
 			setTimeout(()=>{
-				 // this.showLoading()
-				 // this.getPermissionByPosition('add',this.index_code,result=>{
-					//  this.add=result[0]
-					//  if(result[0]){
-					// 	 this.icon='plusempty'
-					//  }
-				 // })
-				 // this.getGrd()
-				 // this.getDictList()
+				 this.showLoading()
+				 this.getList0()
 			},100)
 			//#ifndef APP-PLUS
 				document.title=""
@@ -267,12 +292,6 @@
 		background: #d0d0d0;
 	}
 	
-	.u-order-title {
-		color: #333333;
-		font-weight: bold;
-		font-size: 32rpx;
-	}
-	
 	.u-order-title.unacive {
 		color: rgb(150, 150, 150);
 	}
@@ -281,11 +300,6 @@
 		color: rgb(150, 150, 150);
 		font-size: 28rpx;
 		margin-bottom: 6rpx;
-	}
-	
-	.u-order-time {
-		color: rgb(200, 200, 200);
-		font-size: 26rpx;
 	}
 	
 	.tel {
@@ -308,31 +322,73 @@
 		width: 30px;
 		height: 30px;
 	}
-	.content{
-		margin: 44px 0 2px;
-	}
+	
 	.slot-text {
 		flex: 1;
 		font-size: 14px;
 		margin-right: 10px;
 	}
+	
+	
+	
+	
+	
+	.u-order-time {
+		color: rgba(144,144,144);
+		font-size: 26rpx;
+	}
+	.u-order-detail{
+		color: rgba(144,144,144);
+		font-size:26rpx;
+		margin-bottom: 5px;
+		word-break: break-all;
+	}
+	.content{
+		margin: 44px 0 2px;
+	}
 	::v-deep  .u-time-axis::before{
-		border-left: 1px solid #00CFBD;
+		border-left: 1px solid #00CFBD !important;
 	}
 	::v-deep .u-dot {
-		background: #00CFBD;
+		background: #00CFBD !important;
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	.u-order-right{
+		color: red;
+		border: 1px solid red;
+		border-radius: 50%;
+		padding:2px ;
+		font-size: 12px;
+		text-align: center;
+		width: 15px;
+		margin-right: 5px;
+	}
+	.u-order-right-txt{
+		color: rgba(144,144,144);
+		font-size: 26rpx;
+		display: flex;
+		align-items: center;
+		justify-content: flex-end;
+	}
+	.u-order-title,.u-order-title1,.u-order-title2 {
+		margin: 5px 0;
+		font-size: 13px;
+		height: 30px !important;
+		width: 70px;
+		text-align: center;
+		background: #ff9900;
+		border-radius: 5px;
+		color: white;
+		display: flex;
+		align-items: center;
+		justify-content: space-around;
+	}
+	.u-order-title1{
+		height:25px !important;
+		width: 55px;
+	}
+	.u-order-title2{
+		background: #1AB501;
+	}
 	.line{
 		height: 1px;
 		background-color: #e5e5e5;
