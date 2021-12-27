b99e4ee37c06ec6fb732577a564871eb78f30c4a
LuckyCX <721685743@qq.com>
2021-12-27 14:17:53 +0800

    no message

--- pages/schapp_work/ketangAddStu.vue
+++ pages/schapp_work/ketangAddStu.vue
@@ -133,15 +133,30 @@
 					}
 				})
 				if(canSubmit){
-					if(this.tabBarItem.historyData){
-						this.$refs.alertDialog.open()
-						this.canSub=false
-					}else{
-						if(this.canSub){
-							this.canSub=false
-							this.showLoading()
-							this.submitData()
+					let stuList=[]
+					this.stuList.map(stuItem=>{
+						if(stuItem.item_code=='*' || stuItem.item_code=='**'){}else{
+							let obj={
+								stu_code:stuItem.value,
+								stu_name:stuItem.name,
+								item_code:stuItem.item_code,
+							}
+							stuList.push(obj)
 						}
+					})
+					if(stuList.length>0){
+						if(this.tabBarItem.historyData){
+							this.$refs.alertDialog.open()
+							this.canSub=false
+						}else{
+							if(this.canSub){
+								this.canSub=false
+								this.showLoading()
+								this.submitData()
+							}
+						}
+					}else{
+						this.$refs.alertDialog2.open()
 					}
 				}else{
 					this.showToast('请将考勤情况填写完整再保存！')
@@ -169,59 +184,43 @@
 				})
 			},
 			submitData(){
-				let stuList=[]
-				this.stuList.map(stuItem=>{
-					if(stuItem.item_code=='*' || stuItem.item_code=='**'){}else{
-						let obj={
-							stu_code:stuItem.value,
-							stu_name:stuItem.name,
-							item_code:stuItem.item_code,
-						}
-						stuList.push(obj)
-					}
-				})
-				if(stuList.length>0){
-					let comData={
-						grd_code:this.tabBarItem.grd.value,
-						grd_name:this.tabBarItem.grd.text,
-						cls_code:this.tabBarItem.cls.value,
-						cls_name:this.tabBarItem.cls.text,
-						attendance_time:this.tabBarItem.time,
-						class_node:this.tabBarItem.jc.value,
-						sub_code:this.tabBarItem.km.value,
-						sub_name:this.tabBarItem.km.text,
-						comment:'',
-						list:stuList,
-						index_code:this.index_code,
-					}
-					console.log("comData: " + JSON.stringify(comData));
-					//113.课堂考勤-按班级新增
-					this.post(this.globaData.INTERFACE_WORK+'StudentAttendance/saveList',comData,(response0,response)=>{
-					    console.log("responseaaaa: " + JSON.stringify(response));
-						if (response.code == 0) {
-							 this.hideLoading()
-							 this.showToast(response.msg);
-							 var pages = getCurrentPages();
-							 let pageIndex=1
-							 pages.map((item,index)=>{
-							 	 if(item.route.indexOf('pages/schapp_work/ketangIndex')!==-1){
-							 		 pageIndex=(pages.length-1)-index
-							 	 }
-							 })
-							 uni.$emit('refreshKetangList', {data: 1});
-							 uni.navigateBack({delta:pageIndex});
-						} else {
-							this.canSub=true
-							this.hideLoading()
-							this.showToast(response.msg);
-						}
-					},()=>{
-							this.canSub=true
-					})
-				}else{
-					this.hideLoading()
-					this.$refs.alertDialog2.open()
+				let comData={
+					grd_code:this.tabBarItem.grd.value,
+					grd_name:this.tabBarItem.grd.text,
+					cls_code:this.tabBarItem.cls.value,
+					cls_name:this.tabBarItem.cls.text,
+					attendance_time:this.tabBarItem.time,
+					class_node:this.tabBarItem.jc.value,
+					sub_code:this.tabBarItem.km.value,
+					sub_name:this.tabBarItem.km.text,
+					comment:'',
+					list:stuList,
+					index_code:this.index_code,
 				}
+				console.log("comData: " + JSON.stringify(comData));
+				//113.课堂考勤-按班级新增
+				this.post(this.globaData.INTERFACE_WORK+'StudentAttendance/saveList',comData,(response0,response)=>{
+					console.log("responseaaaa: " + JSON.stringify(response));
+					if (response.code == 0) {
+						 this.hideLoading()
+						 this.showToast(response.msg);
+						 var pages = getCurrentPages();
+						 let pageIndex=1
+						 pages.map((item,index)=>{
+							 if(item.route.indexOf('pages/schapp_work/ketangIndex')!==-1){
+								 pageIndex=(pages.length-1)-index
+							 }
+						 })
+						 uni.$emit('refreshKetangList', {data: 1});
+						 uni.navigateBack({delta:pageIndex});
+					} else {
+						this.canSub=true
+						this.hideLoading()
+						this.showToast(response.msg);
+					}
+				},()=>{
+						this.canSub=true
+				})
 			},
 			yidao(){
 				let stuList=this.stuList
